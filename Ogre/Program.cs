10f9be9b05b1f09b8f711c794fb40248f1d4685e
thisu1930469 <thisu1930469@etucegepjonquiere.ca>
2021-02-02 15:44:29 -0500

    Ajoutez des fichiers projet.

---
+++ Ogre/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ogre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
